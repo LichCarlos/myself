b4537a4c7e755bf4ec7cd94a138a25741e7b94cf
LichCarlos <1434419455@qq.com>
2025-10-07 23:21:42 +0800

    修改简历

--- i18n.ts
+++ i18n.ts
@@ -1,19 +1,4 @@
-// import { notFound } from 'next/navigation';
-// import { getRequestConfig } from 'next-intl/server';
 
-// // Can be imported from a shared config
-// const locales = ['en', 'zh'];
-
-// export default getRequestConfig(async ({ locale }) => {
-//     // Validate that the incoming `locale` parameter is valid
-//     if (!locales.includes(locale as any)) notFound();
-
-//     return {
-//         messages: (await import(`./messages/${locale}.json`)).default
-//     };
-// });
-
-import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
 // Can be imported from a shared config
@@ -22,7 +7,7 @@ const locales = ['en', 'zh'];
 export default getRequestConfig(async ({ locale }) => {
     // 如果请求的locale无效或者为空，强制使用'en'
     if (!locale || !locales.includes(locale as any)) {
-        locale = 'en'; // 强制设置为英文
+        locale = 'zh'; // 强制设置为中文
     }
 
     return {
--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,3 @@
-// import createMiddleware from 'next-intl/middleware';
-
-// export default createMiddleware({
-//     // A list of all locales that are supported
-//     locales: ['en', 'zh'],
-
-//     // Used when no locale matches
-//     defaultLocale: 'en'
-// });
-
-// export const config = {
-//     // Match only internationalized pathnames
-//     matcher: ['/', '/(zh|en)/:path*']
-// };
-
 import { NextRequest, NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 
